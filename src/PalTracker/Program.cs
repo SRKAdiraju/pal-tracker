167ba34cd39448b05720377e5d3fd16f60c1fb59
Mike Gehard <mgehard@pivotal.io>
2018-06-13 09:14:20 -0600

    DataIntegration1.1

--- src/PalTracker/Program.cs
+++ src/PalTracker/Program.cs
@@ -5,7 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Steeltoe.Extensions.Configuration.CloudFoundry; 
+
 
 namespace PalTracker
 {
@@ -18,6 +19,7 @@ namespace PalTracker
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .AddCloudFoundry()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
